4b69daca483ae37ae1bafe654cae3b2e003accaa
Paul Esch-Laurent <herppfel@gmail.com>
2017-05-24 00:58:32 -0400

    Add RNG distribution statistics to response

--- api/utilities.php
+++ api/utilities.php
@@ -2,16 +2,57 @@
 
 function rng($lower, $upper, $times) {
 
-  $numbers = [];
+  $values = [];
 
   for ($i = 0; $i < $times; $i++) {
 
-    $numbers[] = random_int($lower, $upper);
+    $values[] = random_int($lower, $upper);
 
   }
 
+  $sorted = $values;
+
+  sort($sorted);
+
   return [
-    'numbers' => $numbers
+    'values' => $values,
+    'statistics' => [
+      'minimum' => min($values),
+      'maximum' => max($values),
+      'mean'    => mean($values),
+      'median'  => median($sorted)
+    ]
   ];
 
+}
+
+function mean($array) {
+  return array_sum($array) / count($array);
+}
+
+function median($array) {
+
+  $length = count($array);
+
+  // middle value (if odd)
+  // middle - 1 value (if even)
+  $mid = floor(($length - 1) / 2);
+
+  // odd number of elements
+  if ($length % 2) {
+
+    // just take middle value
+    $median = $array[$mid];
+
+  // even number of elements
+  } else {
+
+    // average the two middle values
+    $low = $array[$mid];
+    $high = $array[$mid + 1];
+    $median = ($low + $high) / 2;
+
+  }
+
+  return $median;
 }
