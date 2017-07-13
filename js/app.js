f8c33ee946844d289f431c69e8fe291c9536fd76
Paul Esch-Laurent <herppfel@gmail.com>
2017-07-12 21:56:24 -0400

    Restructure JS for readability

--- js/app.js
+++ js/app.js
@@ -15,55 +15,66 @@
       on: "#backplate"
     });
 
-    $("#rnjeesus").on("submit", function(event) {
-      event.preventDefault();
+    $("#rnjeesus").on("submit", generateRNG);
 
-      var lower = $("#lower-bound").val(),
-          upper = $("#upper-bound").val(),
-          quantity = $("#quantity").val();
+    $(".increment, .decrement").on("click", changeValue);
 
-      $.ajax({
-        url: "/api/" + lower + ".." + upper + "@" + quantity,
-        success: function(response) {
-          if (response.status) {
-            $("#results .content").empty();
-            $("#results .content").text(response.data.values.join(", "));
-          } else {
-            $("#results .content").text(response.message);
-          }
-          $("body").css("transform", "translateX(-80%)");
-        },
-        error: function(xhr, status, error) {
-          $("#results .content").html("The RNG genie is not with you today. :(<br>If the issue persists, <a href='https://github.com/Pinjasaur/rnjeesus/issues'>let me know</a>.");
-        }
-      });
-
-      return false;
-    });
-
-    $(".increment, .decrement").on("click", function(event) {
-
-      var $input = $("#" + $(this).data("for")),
-          value = parseInt($input.val(), 10),
-          min = $input.attr("min"),
-          max = $input.attr("max");
-
-      value = ($(this).val() === "+") ? ++value : --value;
-
-      value = Math.min(Math.max(value, min), max);
-
-      $("#" + $(this).data("for")).val(value);
-
-    });
-
-    $(".btn[data-target]").on("click", function(event) {
-
-      var target = $(this).data("target");
-
-      $("body").css("transform", "translateX(" + ((target - 1) * -20) + "%)");
-
-    });
+    $(".btn[data-target]").on("click", goToSection);
 
   });
+
+  // Submit the form and make an AJAX request to the API
+  function generateRNG(event) {
+
+    event.preventDefault();
+
+    var lower = $("#lower-bound").val().trim(),
+        upper = $("#upper-bound").val().trim(),
+        quantity = $("#quantity").val().trim();
+
+    $.ajax({
+      url: "/api/" + lower + ".." + upper + "@" + quantity,
+      success: function(response) {
+        if (response.status) {
+          $("#results .content").empty();
+          $("#results .content").text(response.data.values.join(", "));
+        } else {
+          $("#results .content").text(response.message);
+        }
+        $("body").css("transform", "translateX(-80%)");
+      },
+      error: function(xhr, status, error) {
+        $("#results .content").html("The RNG genie is not with you today. :(<br>If the issue persists, <a href='https://github.com/Pinjasaur/rnjeesus/issues'>let me know</a>.");
+      }
+    });
+
+    return false;
+
+  }
+
+  // Change the value to an number <input>
+  function changeValue(event) {
+
+    var $input = $("#" + $(this).data("for")),
+        value = parseInt($input.val(), 10),
+        min = $input.attr("min"),
+        max = $input.attr("max");
+
+    value = ($(this).val() === "+") ? ++value : --value;
+
+    value = Math.min(Math.max(value, min), max);
+
+    $("#" + $(this).data("for")).val(value);
+
+  }
+
+  // Animate to a section
+  function goToSection(event) {
+
+    var target = $(this).data("target");
+
+    $("body").css("transform", "translateX(" + ((target - 1) * -20) + "%)");
+
+  }
 
 })(jQuery, window, document);
